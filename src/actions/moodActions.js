531797fca16f1b20ae7f724e14ab8d557a42075f
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-12 15:42:09 -0700

    scaffold moodActions

--- src/actions/moodActions.js
+++ src/actions/moodActions.js
@@ -1,7 +1,27 @@
-export const ADD_MOOD = 'ADD_MOOD';
-export const addMood = (mood) => ({
-  type: ADD_MOOD,
+export const DRINK_COFFEE = 'DRINK_COFFEE';
+export const drinkCoffee = () => ({
+  type: DRINK_COFFEE,
   payload: {
-    mood
+  }
+});
+
+export const EAT_SNACK = 'EAT_SNACK';
+export const eatSnack = () => ({
+  type: EAT_SNACK,
+  payload: {
+  }
+});
+
+export const TAKE_NAP = 'TAKE_NAP';
+export const takeNap = () => ({
+  type: TAKE_NAP,
+  payload: {
+  }
+});
+
+export const STUDY = 'STUDY';
+export const study = () => ({
+  type: STUDY,
+  payload: {
   }
 });
