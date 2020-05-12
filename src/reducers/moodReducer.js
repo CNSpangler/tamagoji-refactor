910799dd60e65087c810e719f8e7abd5842493cc
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-12 15:56:05 -0700

    convert switch to a reducer file

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -1,6 +1,18 @@
-export default function reducer(state = [], action) {
+import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from "../actions/moodActions";
+
+const initialState = [];
+
+export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case DRINK_COFFEE:
+      return [...state, action.payload];
+    case EAT_SNACK:
+      return [...state, action.payload];
+    case TAKE_NAP:
+      return [...state, action.payload];
+    case STUDY:
+      return [...state, action.payload];
     default:
-      return state;
+      return `unhandled type: ${action.type}`;
   }
 }
