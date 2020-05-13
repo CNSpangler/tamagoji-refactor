8fa8355f22a2c058ad940d923ebbb8badfa605a7
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-12 17:27:14 -0700

    two more passing selector tests

--- src/selectors/moodSelectors.test.js
+++ src/selectors/moodSelectors.test.js
@@ -8,4 +8,21 @@ describe('mood selectors', () => {
 
     expect(face).toEqual('🤬');
   });
+  
+  it('returns barf if overstimulated', () => {
+    const state = { coffees: 6, snacks: 0 };
+
+    const face = getFace(state);
+
+    expect(face).toEqual('🤮');
+  });
+
+  it('returns sleeping face if tired', () => {
+    const state = { coffees: 0, naps: 0 };
+
+    const face = getFace(state);
+
+    expect(face).toEqual('😴');
+  });
+
 });
