7b44e69516aed3c8083c7c52512483eb7f7d5b5c
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-12 17:50:08 -0700

    move useSelector to Face, change Moods to function component

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
-import { getFace } from '../selectors/moodSelector';
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from '../actions/moodActions';
 
-export default class Moods extends Component {
+const Moods = () => {
+  const dispatch = useDispatch();
 
-  render() {
-    const face = useSelector(getFace);
+  return (
+    <>
+      <Controls>
+        <button onClick={() => dispatch({ type: DRINK_COFFEE })}>coffee - {coffees}</button>
+        <button onClick={() => dispatch({ type: EAT_SNACK })}>snacks - {snacks}</button>
+        <button onClick={() => dispatch({ type: TAKE_NAP })}>naps - {naps}</button>
+        <button onClick={() => dispatch({ type: STUDY })}>studies - {studies}</button>
+      </Controls>
+      <Face />
+    </>
+  );
+};
 
-    return (
-      <>
-        <Controls>
-          <button onClick={() => this.handleSelection({ type: 'DRINK_COFFEE' })}>coffee - {coffees}</button>
-          <button onClick={() => this.handleSelection({ type: 'EAT_SNACK' })}>snacks - {snacks}</button>
-          <button onClick={() => this.handleSelection({ type: 'TAKE_NAP' })}>naps - {naps}</button>
-          <button onClick={() => this.handleSelection({ type: 'STUDY' })}>studies - {studies}</button>
-        </Controls>
-        <Face emoji={face} />
-      </>
-    );
-  }
-}
+export default Moods;
